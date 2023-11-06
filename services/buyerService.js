9b9364817b36082548ab65d0c6845016a731b2bc
navya <navya@metrictreelabs.com>
2023-11-06 12:55:12 +0530

    error handling added in buyer service

--- services/buyerService.js
+++ services/buyerService.js
@@ -1,11 +1,21 @@
 const BuyerModel = require('../models/buyer');
 module.exports = {
   updateBuyerProfile: async (data) => {
-    let { name, phoneNumber } = data;
-    await BuyerModel.findOneAndUpdate(phoneNumber, { name });
+   try{ let { name, phoneNumber } = data;
+    await BuyerModel.findOneAndUpdate(phoneNumber, { name });}
+    catch(e)
+    {
+      throw new Error(e)
+    }
   },
   completeBuyerProfile: async (data) => {
-    let { name, phoneNumber } = data;
-    await BuyerModel.findOneAndUpdate(phoneNumber, { name });
-  },
-};
+ try {  let { name, phoneNumber } = data;
+    await BuyerModel.findOneAndUpdate(phoneNumber, { name })
+  }
+  catch(e)
+  {
+    throw new Error(e)
+
+  }
+}
+}
