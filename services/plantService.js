d76a32c3ce4c6c77685f0cd4da65bd9a2dbd0212
navya <mail.navyaraj@gmail.com>
2024-07-16 21:43:07 +0530

    seeder: path changed

--- services/plantService.js
+++ services/plantService.js
@@ -7,8 +7,9 @@ module.exports = {
   bulkInserCateories: async () => {
     const data = JSON.parse(
       fs.readFileSync(
-        '/home/metrictreelabs/Desktop/MYWorks/Plantnet/seeder/plantCategories.json',
+        '/home/metric/Desktop/My Projects/plantnet/seeder/plantCategories.json',
         'utf8',
+
       ),
     );
     plantCategoriesModel.insertMany(data);
