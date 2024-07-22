0a9bc1ae0643c910d948939cb21066001b6887aa
navya <mail.navyaraj@gmail.com>
2024-07-22 22:27:50 +0530

    plant: controllers and services updated with new endpoints

--- controllers/plantController.js
+++ controllers/plantController.js
@@ -16,13 +16,78 @@ module.exports = {
       return next(error);
     }
   },
-  // plant details
-  // plant statistics
-  // edit plant
+  deletePlant: async (req, res, next) => {
+    try {
+      let response = await plantService.deletePlantFromSale(req.body);
+      return res.status(200).json({ response });
+    } catch (error) {
+      return next(error);
+    }
+  },
+  fetchPlantDetails: async (req, res, next) => {
+    try {
+      let response = await plantService.fetchPlantDetails(req.query);
+      return res.status(200).json({ response });
+    } catch (error) {
+      return next(error);
+    }
+  },
+  fetchPlantStatistic: async (req, res, next) => {
+    try {
+      let response = await plantService.fetchPlantStatistic(req.query);
+      return res.status(200).json({ response });
+    } catch (error) {
+      return next(error);
+    }
+  },
+  updatePlant: async (req, res, next) => {
+    try {
+      let response = await plantService.updatePlantDetails(req.body);
+      return res.status(200).json({ response });
+    } catch (error) {
+      return next(error);
+    }
+  },
+  updatePlantStock: async (req, res, next) => {
+    try {
+      let response = await plantService.updateStock(req.body);
+      return res.status(200).json({ response });
+    } catch (error) {
+      return next(error);
+    }
+  },
+  updatePlantStatus: async (req, res, next) => {
+    try {
+      let response = await plantService.updatePlantAsOutofStock(req.body);
+      return res.status(200).json({ response });
+    } catch (error) {
+      return next(error);
+    }
+  },
+  listOrders: async (req, res, next) => {
+    try {
+      let userId=req.user.id
+      let response = await plantService.listOrders(userId);
+      return res.status(200).json({ response });
+    } catch (error) {
+      return next(error);
+    }
+  },
+  fetchOrderDetails: async (req, res, next) => {
+    try {
+      let response = await plantService.fetchOrderDetails(req.user.id);
+      return res.status(200).json({ response });
+    } catch (error) {
+      return next(error);
+    }
+  },
+  updateOrderStatus: async (req, res, next) => {
+    try {
+      let response = await plantService.updateOrderStatus(req.body);
+      return res.status(200).json({ response });
+    } catch (error) {
+      return next(error);
+    }
+  },
   // audio video upload
-  // orders list
-  // order details
-  // order accept
-  // order status updation
-  
 };
