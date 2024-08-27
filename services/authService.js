2bac0239db6b112ddd49bffdc718e2023bcf6009
navya <mail.navyaraj@gmail.com>
2024-08-27 22:40:05 +0530

    auth: code refactor

--- services/authService.js
+++ services/authService.js
@@ -2,81 +2,47 @@ const { genSalt, hash, compare } = require('bcrypt');
 const UserModel = require('../models/user');
 const OtpModel = require('../models/otp');
 const otpService = require('../helper/otp-service');
-const SellerModel = require('../models/seller');
-const BuyerModel = require('../models/buyer');
 const { generateToken } = require('../helper/jwt.service');
 const { BadRequestError } = require('../exceptions/requestException');
-const { findUserByPhoneNumber } = require('../services/userService')
+const { findUserByPhoneNumber, checkUserVerifiedByNumber, checkUnverifiedUserByNumber, createUser } = require('../services/userService')
 module.exports = {
   signin: async (data) => {
     try {
-      let { phoneNumber, password } = data;
-      let isUserVerified = await UserModel.findOne({
-        phoneNumber,
-        isVerified: true,
-      });
-      if (isUserVerified) {
-        let user = await UserModel.findOne({ phoneNumber });
-        let isPasswordValid = await compare(password, user.password);
-        if (isPasswordValid) {
-          if (user.userType == 'seller') {
-            let sellerData = await SellerModel.findOne({ userId: user.id });
-            if (!sellerData) {
-              await SellerModel.create({ userId: user.id, phoneNumber });
-            }
-          }
-           else {
-            let buyerData = await BuyerModel.findOne({ userId: user.id });
-            if (!buyerData) await BuyerModel.create({ userId: user.id, phoneNumber }); 
-          }
-          let token = await generateToken(user.id);
-          return { message: 'Happy Signin to Plantnet', token };
-        } else throw new Error('Invalid Password');
-        
-      } 
-      else  throw new Error(`Your Phone number is not verified :( Please Signup and verify your account`);
-                      
+      const { phoneNumber, password } = data;
+      const isUserVerified = await checkUserVerifiedByNumber(phoneNumber)
+      if (!isUserVerified) throw new Error(`Your Phone number is not verified :( , Please Signup and verify your account`);
+      const user = await findUserByPhoneNumber(phoneNumber);
+      const isPasswordValid = await compare(password, user.password);
+      if (!isPasswordValid) throw new Error('Invalid Password');
+      let token = await generateToken(user.id);
+      return { message: 'Happy Signin to Plantnet', token };
     } catch (error) {
+      console.log("Error", error);
       throw new BadRequestError(error);
     }
   },
   signup: async (data) => {
+
     try {
       let { phoneNumber, userType, password } = data;
-      let salt = await genSalt(10);
+      const salt = await genSalt(10);
       password = await hash(password, salt);
-      let isExists = await UserModel.findOne({
-        phoneNumber,
-        isVerified: false,
-      });
+
+      const isExists = await checkUnverifiedUserByNumber(phoneNumber)
       if (isExists) return 'An OTP Send to Your Phone Number, Please Verify ';
-      
-      let isVerified = await UserModel.findOne({
-        phoneNumber,
-        isVerified: true,
-      });
+
+      const isVerified = await checkUserVerifiedByNumber(phoneNumber)
       if (isVerified) return 'You already in our Plantnet, Please signin';
-      
-      let otp = Math.floor(Math.random() * 123456);
-      let isOtpGenerated = await OtpModel.findOne({ phoneNumber });
-      if (isOtpGenerated == null) {
-        await OtpModel.create({
-          phoneNumber,
-          otp,
-        });
-      } else {
-        await OtpModel.updateOne({
-          phoneNumber,
-          otp,
-        });
-      }
+
+      const otp = Math.floor(Math.random() * 123456);
+      const isOtpGenerated = await OtpModel.findOne({ phoneNumber });
+
+      if (isOtpGenerated == null) await OtpModel.create({ phoneNumber, otp });
+      else await OtpModel.updateOne({ phoneNumber, otp });
+
       await otpService.sendOtp(phoneNumber, otp);
-      let user = await UserModel.create({
-        phoneNumber,
-        userType,
-        password,
-      });
-      return user;
+      return await createUser(phoneNumber, userType, password)
+
     } catch (error) {
       throw new BadRequestError(error);
     }
--- services/userService.js
+++ services/userService.js
@@ -1,4 +1,5 @@
 const UserModel = require('../models/user');
+const { BadRequestError } = require('../exceptions/requestException');
 module.exports = {
   uploadProfilePicture: async (fileData, userId) => {
     try {
@@ -10,7 +11,39 @@ module.exports = {
   },
   findUserByPhoneNumber: async (phoneNumber) => {
     try {
-    return await UserModel.findOne({ phoneNumber });
+      return await UserModel.findOne({ phoneNumber });
+    } catch (e) {
+      throw new BadRequestError(e);
+    }
+  },
+  checkUserVerifiedByNumber: async (phoneNumber) => {
+    try {
+      return await UserModel.findOne({ phoneNumber },'isVerified');
+    } catch (e) {
+      throw new BadRequestError(e);
+    }
+  },
+  getUserById: async (userId) => {
+    try {
+      return await UserModel.findOne({ userId }, 'isVerified');
+    } catch (e) {
+      throw new BadRequestError(e);
+    }
+  },
+  checkUnverifiedUserByNumber: async (phoneNumber) => {
+    try {
+      return await UserModel.findOne({ phoneNumber, isVerified:false });
+    } catch (e) {
+      throw new BadRequestError(e);
+    }
+  },
+  createUser: async (phoneNumber,userType,password) => {
+    try {
+      return await UserModel.create({
+        phoneNumber,
+        userType,
+        password,
+      });
     } catch (e) {
       throw new BadRequestError(e);
     }
